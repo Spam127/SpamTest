4e5c0c579be6907505e08f0a11823cc462f6acc4
Spam127 <45201082+Spam127@users.noreply.github.com>
2018-12-05 11:59:01 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+client.on('ready', async() => {
+var server = "Id server"; // "519471264348504084"
+var channel = "id room";//"519471264348504086"
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam,RoYaleGameSpam**')
+    },305);
+})
+
+client.login("");
